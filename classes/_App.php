bcfca60a33de680eed633f0cee37a3f6eccbb4b4
moritzebeling <git@moritzebeling.com>
2020-03-30 11:52:47 +0200

    improve dump() helper function

--- classes/_App.php
+++ classes/_App.php
@@ -5,7 +5,15 @@ require_once 'Time.php';
 require_once 'Storage.php';
 
 function dump( $variable ){
-  echo '<pre>' . print_r($variable, true) . '</pre>';
+  echo '<pre style="margin: 1rem 0; padding: 1rem; border: 1px solid #ccc;">';
+
+    $backtrace = debug_backtrace();
+    echo '<div style="color:#ccc;">';
+    echo $backtrace[0]['file'] . ' at line ' . $backtrace[0]['line'];
+    echo '</div>';
+
+  echo print_r($variable, true);
+  echo '</pre>';
 }
 
 class App {
